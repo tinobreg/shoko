11d7fc8d79e765fdc7b742dd338711d5ed865d75
Martin Obregon <martin@flydreamers.com>
2017-03-24 21:06:50 -0300

    update

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=shoko_database',
+            'username' => 'tinobreg_shoko',
+            'password' => 'ShokoBsAs2017',
             'charset' => 'utf8',
         ],
         'mailer' => [
